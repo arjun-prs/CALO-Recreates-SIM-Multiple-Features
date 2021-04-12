615b90d65a6b348256a761005e80fb8850408127
Arjun R <arjun.prs@gmail.com>
2021-04-12 10:27:04 +0530

    resfilter optimization

--- Code/calo.py
+++ Code/calo.py
@@ -7,105 +7,103 @@ print("\n" + __copyright__)
 
 
 def exbridgedomain(result) -> list:
-    domain_list=[]
+    domain_list = []
     for segments in result:
         segment = segments.split("\n")
         for line in segment:
             if re.search("BDI", line):
-                line=line.strip()
-                temp=line[-3:]
-                domain_list.append("bridge-domain "+temp)
+                line = line.strip()
+                temp = line[-3:]
+                domain_list.append("bridge-domain " + temp)
     return domain_list
 
 
-
 def exbdiinterface(lines, result, slist) -> list:
-    scount=0
-    shcount,dhcount=exreq(lines,"show running-config")
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+    scount = 0
+    shcount, dhcount = exreq(lines, "show running-config")
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in slist:
             if re.search(sparam, lines[i]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
+                    tresult += lines[j]
                 if tresult not in result and re.search("interface", tresult):
                     result.append(tresult)
     return result
-    
 
 
 def resfilter(result, keyword) -> list:
-    master_result=[]
+    master_result = []
     for lines in result:
-        flag=True
+        flag = True
         for ele in keyword:
             if not re.search(ele, lines):
-                flag=False
-        if flag == True:
+                flag = False
+        if flag:
             master_result.append(lines)
     return master_result
 
 
 def exfilter(lines, result) -> list:
-    scount=0
-    shcount,dhcount=exreq(filename,"show running-config")
-    vlist=[]
+    scount = 0
+    shcount, dhcount = exreq(filename, "show running-config")
+    vlist = []
     for ele in result:
         if re.search("route-map", ele):
-            temp=ele.split(" ")
+            temp = ele.split(" ")
             i = [index for index, value in enumerate(temp) if value == "route-map"]
             for j in i:
-                route=temp[j+1]
-                route=route.strip()
-                vlist.append("route-map "+route)
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+                route = temp[j + 1]
+                route = route.strip()
+                vlist.append("route-map " + route)
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in vlist:
             if re.search(sparam, lines[i]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
+                    tresult += lines[j]
                 if tresult not in result:
                     result.append(tresult)
-                    
+
     return result
 
 
 def exumplsbgp(lines, result) -> list:
-    scount=0
-    shcount,dhcount=exreq(lines,"show running-config")
-    slist=["neighbour","session-group","community","community-list"]
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+    scount = 0
+    shcount, dhcount = exreq(lines, "show running-config")
+    slist = ["neighbour", "session-group", "community", "community-list"]
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in slist:
-            if re.search(sparam, lines[i]) and not re.search("snmp",lines[i]) and not re.search("aaa",lines[i]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+            if re.search(sparam, lines[i]) and not re.search("snmp", lines[i]) and not re.search("aaa", lines[i]):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
+                    tresult += lines[j]
                 if tresult not in result:
                     result.append(tresult)
     return result
 
 
-def exmplsbgp(lines) -> list:   
-    count=0
-    result=[]
-    slist=["neighbour","session","community",]
+def exmplsbgp(lines) -> list:
+    count = 0
+    result = []
+    slist = ["neighbour", "session", "community", ]
     for line in lines:
-        count=count+1
+        count = count + 1
         for sparam in slist:
-            if re.search(sparam, line) and re.search("!",lines[count-2]):
-                for lineno in range(count-1,len(lines)):
+            if re.search(sparam, line) and re.search("!", lines[count - 2]):
+                for lineno in range(count - 1, len(lines)):
                     if re.search("!", lines[lineno]):
                         break
                     result.append(lines[lineno])
@@ -114,84 +112,87 @@ def exmplsbgp(lines) -> list:
 
 
 def rexunifiedmpls(lines) -> list:
-    result=[]
+    result = []
     umpls = rexmpls(lines)
     fil = exfilter(lines, umpls)
     ibgp = exumplsbgp(lines, fil)
-    result = result+umpls+fil+ibgp
+    result = result + umpls + fil + ibgp
     return result
 
 
 def exreq(lines, reqs):
-    scount=0
-    count=0
-    dcount=0
+    scount = 0
+    count = 0
+    dcount = 0
     for line in lines:
-        count+=1
-        if re.search(reqs,line):
-            scount=count
+        count += 1
+        if re.search(reqs, line):
+            scount = count
             break
-    for i in range(scount+1,len(lines)):
-        if re.match(r'^end',lines[i]):
-            dcount=i
-            break 
-    return (scount,dcount)
+    for i in range(scount + 1, len(lines)):
+        if re.match(r'^end', lines[i]):
+            dcount = i
+            break
+    return (scount, dcount)
 
 
 def exbgp(lines, result) -> list:
-    scount=0
-    shcount,dhcount=exreq(filename,"show running-config")
-    slist=["bgp","vpnv"]
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+    scount = 0
+    shcount, dhcount = exreq(filename, "show running-config")
+    slist = ["bgp", "vpnv"]
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in slist:
-            if re.search(sparam, lines[i]) and not re.search("bgp-community",lines[i]) and not re.search("snmp",lines[i]):
-                tresult="\n!"
-                for j in range(scount+1,dhcount):
+            if re.search(sparam, lines[i]) and not re.search("bgp-community", lines[i]) and not re.search("snmp",
+                                                                                                          lines[i]):
+                tresult = "\n!"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
-                if tresult not in result and not re.search("MGMT",tresult):
-                    result.append(tresult)     
+                    tresult += lines[j]
+                if tresult not in result and not re.search("MGMT", tresult):
+                    result.append(tresult)
     return result
 
 
 def exigp(lines) -> list:
-    result=[]
-    scount=0
-    shcount,dhcount=exreq(lines,"show running-config")
-    slist=["ospf","isis","eigrp","ip route"]
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+    result = []
+    scount = 0
+    shcount, dhcount = exreq(lines, "show running-config")
+    slist = ["ospf", "isis", "eigrp", "ip route"]
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in slist:
-            if re.search(sparam, lines[i]) and not re.search("snmp",lines[i]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+            if re.search(sparam, lines[i]) and not re.search("snmp", lines[i]):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
+                    tresult += lines[j]
                 if tresult not in result:
                     result.append(tresult)
     return result
 
 
 def exmpls(lines, result):
-    scount=0
-    shcount,dhcount=exreq(lines,"show running-config")
-    slist=["mpls"]
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+    scount = 0
+    shcount, dhcount = exreq(lines, "show running-config")
+    slist = ["mpls"]
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in slist:
-            if re.search(sparam, lines[i]) and not re.search("snmp",lines[scount+2]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+            if re.search(sparam, lines[i]) and not re.search("snmp", lines[scount + 2]):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
-                if tresult not in result and not re.search("encapsulation",tresult) and not re.search("MGMT",tresult) and not re.search("qos",tresult):
+                    tresult += lines[j]
+                if tresult not in result and not re.search("encapsulation", tresult) and not re.search("MGMT",
+                                                                                                       tresult) and not re.search(
+                        "qos", tresult):
                     result.append(tresult)
     return result
 
@@ -210,26 +211,26 @@ def rexmpls(lines) -> list:
 
 
 def exvrfint(lines, result) -> list:
-    scount=0
-    shcount,dhcount=exreq(filename,"show running-config")
-    vlist=[]
+    scount = 0
+    shcount, dhcount = exreq(filename, "show running-config")
+    vlist = []
     for ele in result:
-        if re.search("vrf forwarding",ele):
-            temp=ele.split(" ")
+        if re.search("vrf forwarding", ele):
+            temp = ele.split(" ")
             i = temp.index("vrf")
-            vlist.append("vrf definition "+temp[i+2])
-    for i in range(shcount,dhcount):
-        if re.search("!",lines[i]):
-            scount=i
+            vlist.append("vrf definition " + temp[i + 2])
+    for i in range(shcount, dhcount):
+        if re.search("!", lines[i]):
+            scount = i
         for sparam in vlist:
             if re.search(sparam, lines[i]):
-                tresult="!\n"
-                for j in range(scount+1,dhcount):
+                tresult = "!\n"
+                for j in range(scount + 1, dhcount):
                     if re.search("!", lines[j]):
                         break
-                    tresult+=lines[j]
-                if tresult not in result and not re.search("MGMT",tresult) and not re.search("mgmt",tresult):
-                    result.append(tresult)           
+                    tresult += lines[j]
+                if tresult not in result and not re.search("MGMT", tresult) and not re.search("mgmt", tresult):
+                    result.append(tresult)
     return result
 
 
@@ -412,36 +413,36 @@ def multicast_json(lines) -> list:
 
 
 u = "y"
-while (u == "y"):
-    result=[]
+while u == "y":
+    result = []
     user_input0 = input("Do you need optimised filter or not (y/n): ")
-    if(user_input0 == "y"):
-         user_input2 = input(
-                "Which running configuration do you need: (igp/mpls/unified_mpls/multicast) ")
-         filename = 'Input\\mpls config.txt'
-         f = open(filename, 'r')
-         lines = f.readlines()
-         if user_input2 == "igp":
-             result = exigp(lines)
-         elif user_input2 == "mpls":
-             result = rexmpls(lines)
-         elif user_input2 == "unified_mpls":
-             result = rexunifiedmpls(lines)
-         elif user_input2 == "multicast":
-             result.append("Feature still under development")
-         else:
-             result.append("Invalid Input")
-         if result[0] != "Invalid Input":
-                filename = 'Output\\show run (filtered) ' + user_input2 + '.txt'
-                with open(filename, 'w+') as fout:
-                    for j in result:
-                        fout.write(j)
+    if user_input0 == "y":
+        user_input2 = input(
+            "Which running configuration do you need: (igp/mpls/unified_mpls/multicast) ")
+        filename = 'Input\\mpls config.txt'
+        f = open(filename, 'r')
+        lines = f.readlines()
+        if user_input2 == "igp":
+            result = exigp(lines)
+        elif user_input2 == "mpls":
+            result = rexmpls(lines)
+        elif user_input2 == "unified_mpls":
+            result = rexunifiedmpls(lines)
+        elif user_input2 == "multicast":
+            result.append("Feature still under development")
+        else:
+            result.append("Invalid Input")
+        if result[0] != "Invalid Input":
+            filename = 'Output\\show run (filtered) ' + user_input2 + '.txt'
+            with open(filename, 'w+') as fout:
+                for j in result:
+                    fout.write(j)
     else:
         user_input1 = input("Do you need to filter running configurartion or ip route configuration (run/route): ")
         if user_input1 == "run":
             user_input2 = input(
                 "Which running configuration do you need: (bgp/isis/eigrp/ospf/mpls/unified_mpls/multicast/interface) ")
-            #filename = 'Input\\show run ' + user_input2 + '.txt'
+            # filename = 'Input\\show run ' + user_input2 + '.txt'
             filename = 'Input\\mpls config.txt'
             f = open(filename, 'r')
             lines = f.readlines()
@@ -498,11 +499,11 @@ while (u == "y"):
     if user_input3 == "y":
         keyword = input("Please enter keywords in space seperated format: ")
         keywords = keyword.split()
-        filtered_result=resfilter(result, keywords)
+        filtered_result = resfilter(result, keywords)
         print(*filtered_result, sep="\n")
         with open("Output\\filtered result.txt", 'w+') as fout:
-                    for j in filtered_result:
-                        fout.write(j)
+            for j in filtered_result:
+                fout.write(j)
     u = input("Do you want to continue (y/n): ")
 f.close()
 fout.close()
